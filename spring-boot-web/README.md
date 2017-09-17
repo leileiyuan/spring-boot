d8f9f4110843b8f4fd10314914095675a161a256
Administrator <yuanleilei@163.com>
2017-09-17 15:31:17 +0800

    spring-boot-web应用

---
+++ spring-boot-web/README.md
@@ -0,0 +1,4 @@
+1.thymeleaf模块引擎整合spring mvc
+2.websocket 使用
+3.bootstrap angularJS 现代web应用
+。。。。。。
---
+++ spring-boot-web/src/main/java/com/belongtou/App.java
@@ -0,0 +1,11 @@
+package com.belongtou;/**
+ * Created by Administrator on 2017/9/17.
+ */
+
+/**
+ * @author yll
+ * @Description
+ * @Date 2017-09-17
+ **/
+public class App {
+}
