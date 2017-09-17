a949859caf77f836d6732c74bff366818e8fb91e
Administrator <yuanleilei@163.com>
2017-09-17 16:03:08 +0800

    spring boot jpa

---
+++ spring-boot-database/src/main/java/com/belongtou/App.java
@@ -0,0 +1,19 @@
+package com.belongtou;/**
+ * Created by Administrator on 2017/9/17.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yll
+ * @Description
+ * @Date 2017-09-17
+ **/
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
