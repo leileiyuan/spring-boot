b9159dfa02ffbbc065bc9ee995831cd9fc24f022
Administrator <yuanleilei@163.com>
2017-09-19 20:24:02 +0800

    sprinb-boot-websocket

---
+++ sspring-boot-websocket/src/main/java/com/belngtou/SocketMessage.java
@@ -0,0 +1,30 @@
+package com.belngtou;/**
+ * Created by Administrator on 2017/9/19.
+ */
+
+/**
+ * @author yl;
+ * @Description
+ * @Date 2017-09-19
+ **/
+public class SocketMessage {
+    public String message;
+
+    public String date;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ sspring-boot-websocket/src/main/java/com/belngtou/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.belngtou;/**
+ * Created by Administrator on 2017/9/19.
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * @author yll
+ * @Description
+ * @Date 2017-09-19
+ **/
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/my-websocket").withSockJS();
+    }
+}
